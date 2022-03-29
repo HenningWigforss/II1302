a3f1b5b97502114b82349854eb92426b8b29b4bf
Dan Ljungström <danljun@kth.se>
2022-03-29 13:25:03 +0200

    Added translation functions

--- src/morseTest/morseTest.ino
+++ src/morseTest/morseTest.ino
@@ -10,59 +10,117 @@ int speakerFrequency = 880;
 int timeUnit = 150;
 
 void setup() {
-  // put your setup code here, to run once:
-pinMode(redPin, OUTPUT);
-pinMode(greenPin, OUTPUT);
-pinMode(bluePin1, OUTPUT);
-pinMode(bluePin2, OUTPUT);
-pinMode(bluePin3, OUTPUT);
-pinMode(bluePin4, OUTPUT);
-pinMode(bluePin5, OUTPUT);
-pinMode(speaker, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin1, OUTPUT);
+  pinMode(bluePin2, OUTPUT);
+  pinMode(bluePin3, OUTPUT);
+  pinMode(bluePin4, OUTPUT);
+  pinMode(bluePin5, OUTPUT);
+  pinMode(speaker, OUTPUT);
 }
 
-void playDot(){
-  tone(speaker, speakerFrequency); // Send 1KHz sound signal...
-  digitalWrite(redPin, HIGH);   // sets the LED on
-  delay(timeUnit);        // ...for 1 sec
-  noTone(speaker);     // Stop sound...
-  digitalWrite(redPin, LOW);    // sets the LED off
-  delay(timeUnit);        // ...for 1sec
+void playDot() {
+  tone(speaker, speakerFrequency);
+  digitalWrite(redPin, HIGH);
+  delay(timeUnit);
+  noTone(speaker);
+  digitalWrite(redPin, LOW);
+  delay(timeUnit);
 }
 
-void playDash(){
-  tone(speaker, speakerFrequency); // Send 1KHz sound signal...
-  digitalWrite(redPin, HIGH);   // sets the LED on
-  delay(3*timeUnit);        // ...for 1 sec
-  noTone(speaker);     // Stop sound...
-  digitalWrite(redPin, LOW);    // sets the LED off
-  delay(timeUnit);        // ...for 1sec
+void playDash() {
+  tone(speaker, speakerFrequency);
+  digitalWrite(redPin, HIGH);
+  delay(3 * timeUnit);
+  noTone(speaker);
+  digitalWrite(redPin, LOW);
+  delay(timeUnit);
 }
 
-void letterPause(){
-  delay(2*timeUnit);  // An extra delay of 2 time units for each letter(2 since we already have a 1 time unit delay after each char)
+void letterPause() {
+  delay(2 * timeUnit); // An extra delay of 2 time units for each letter(2 since we already have a 1 time unit delay after each char)
 }
 
-void wordPause(){
-  delay(4*timeUnit);  // An extra delay of 2 time units after each word (4 since we already have a 3 time unit delay after each letter)
+void wordPause() {
+  delay(4 * timeUnit); // An extra delay of 2 time units after each word (4 since we already have a 3 time unit delay after each letter)
 }
 
-String morseMessage[] = {"...","--","..."," ","...","---","..."," "};
+String morseAlphabet(char letter) {
+  letter = tolower(letter);
+  String alphabet[][2] = {
+    {"a", ".- "},
+    {"b", "-... "},
+    {"c", "-.-. "},
+    {"d", "-.. "},
+    {"e", ". "},
+    {"f", "..-. "},
+    {"g", "--. "},
+    {"h", ".... "},
+    {"i", ".. "},
+    {"j", ".--- "},
+    {"k", "-.- "},
+    {"l", ".-.. "},
+    {"m", "-- "},
+    {"n", "-. "},
+    {"o", "--- "},
+    {"p", ".--. "},
+    {"q", "--.- "},
+    {"r", ".-. "},
+    {"s", "... "},
+    {"t", "- "},
+    {"u", "..- "},
+    {"v", "...- "},
+    {"w", ".-- "},
+    {"x", "-..- "},
+    {"y", "-.-- "},
+    {"z", "--.. "},
+    {"å", ".--.- "},
+    {"ä", ".-.- "},
+    {"ö", "---. "},
+    {"1", ".---- "},
+    {"2", "..--- "},
+    {"3", "...-- "},
+    {"4", "....- "},
+    {"5", "..... "},
+    {"6", "-.... "},
+    {"7", "--... "},
+    {"8", "---.. "},
+    {"9", "----. "},
+    {"0", "----- "},
+    {" ", ","}
+  };
+
+  for (auto i : alphabet) {
+    if (i[0][0] == letter)
+      return i[1];
+  }
+}
+
+String translateMorse(String message){
+  String morseMessage = "";
+  for(auto i : message){
+    morseMessage += morseAlphabet(i);
+  }
+  return morseMessage;
+}
 
 void loop() {
-  for(auto i : morseMessage){
-    for(auto j : i){
-      if(j == ' '){
-        wordPause();
-      }
-      if(j == '.'){
-        playDot();
-      }
-    
-      if(j == '-'){
-        playDash();
-      }
+  String message = "Arbeta Agilt";
+  String morseMessage = translateMorse(message);
+
+  for (auto i : morseMessage) {
+    if (i == '.') {
+      playDot();
     }
-    letterPause();
+    else if (i == '-') {
+      playDash();
+    }
+    else if (i == ' ') {
+      letterPause();
+    }
+    else if (i == ',') {
+      wordPause();
+    }
   }
 }
