52ffa805b015da103aae42e315e911266855f200
astroxq55 <52320146+astroxq55@users.noreply.github.com>
2022-04-26 11:19:44 +0200

    Update HelloWorld.vue

--- hemsidan/src/components/HelloWorld.vue
+++ hemsidan/src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      check out the AMANDA WAZ H3R3!!!
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
