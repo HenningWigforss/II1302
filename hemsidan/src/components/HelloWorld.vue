8627170d1240bbaa66c145d1f7529152ba05e1ea
Murtadha Alobaidi <mhaao@kth.se>
2022-04-27 20:16:01 +0200

    Update HelloWorld.vue

--- hemsidan/src/components/HelloWorld.vue
+++ hemsidan/src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <!-- <h1>{{ msg }}</h1> -->
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+      AAAFor a guide and recipes on how to configure / customize this project,<br>
       check out the AMANDA WAZ H3R3!!!
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
