089a6d86504a1d55c101d7bf5c050a17b1a9580d
HenningWigforss <33979069+HenningWigforss@users.noreply.github.com>
2022-04-28 12:24:40 +0200

    2
    2

--- src/Server/Controller/ServerController.java
+++ src/Server/Controller/ServerController.java
@@ -1,3 +1,4 @@
+package Server.Controller;
 public class ServerController{
     
 }
--- src/Server/Utility/MessageCheck.java
+++ src/Server/Utility/MessageCheck.java
@@ -5,11 +5,30 @@ package Server.Utility;
     @return boolean true if OK, false if NOT OK
 */
 public class MessageCheck {
+    MAlphabet alp = new MAlphabet();
+    String alphabet[][]= alp.getAlphabet();
 
     public boolean verifyMessage(String msg){
-        if(true){
-            return true;
+        msg.toLowerCase();
+
+        for(char ch : msg.toCharArray()){
+            if(!testChar(ch)) return false;
+        }
+        return true;
+    }
+
+    private boolean testChar(char ch){
+        for(String[] i : alphabet){
+            if(i[0].charAt(0) == ch) return true;
         }
         return false;
+    }
+
+
+    public static void main(String[] args){
+        String str = "Hej detta är ett morsemeddelande";
+        MessageCheck msgCheck = new MessageCheck();
+        boolean check = msgCheck.verifyMessage(str);
+        System.out.println(check);
     }
 }
