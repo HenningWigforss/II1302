9a3cbd4784885aef568cdcc3c3c37b163424b17f
Dan Ljungström <danljun@kth.se>
2022-05-02 11:56:22 +0200

    Update MorseMateHandler.java

--- src/Server/Integration/MorseMateHandler.java
+++ src/Server/Integration/MorseMateHandler.java
@@ -1,5 +1,21 @@
 package Server.Integration;
+import Server.Controller.ServerController;
 
 public class MorseMateHandler {
-    
+    ServerController controller;
+
+    public MorseMateHandler(ServerController c){
+        this.controller = c;
+    }
+
+    private void morseMateListener(){
+        
+    }
+
+    private String fetchMessage(){
+        CommandMessageDTO cmdMessage = controller.fetchCommandMessage();
+        String morseCMD = cmdMessage.CMD + '|' + cmdMessage.numberInQueue + '|' + cmdMessage.morseText;
+
+        return morseCMD;
+    }
 }
