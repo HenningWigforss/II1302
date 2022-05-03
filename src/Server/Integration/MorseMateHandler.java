38d5fd05faac8c755343ec0aa99e597655ee9779
Dan Ljungström <danljun@kth.se>
2022-05-03 13:20:55 +0200

    Update MorseMateHandler.java

--- src/Server/Integration/MorseMateHandler.java
+++ src/Server/Integration/MorseMateHandler.java
@@ -1,6 +1,10 @@
 package Server.Integration;
 import Server.Controller.ServerController;
 
+import Server.DTO.ClientMessageDTO;
+import Server.DTO.CommandMessageDTO;
+import Server.Model.MessageHandler;
+
 public class MorseMateHandler {
     ServerController controller;
 
@@ -13,9 +17,28 @@ public class MorseMateHandler {
     }
 
     private String fetchMessage(){
-        CommandMessageDTO cmdMessage = controller.fetchCommandMessage();
-        String morseCMD = cmdMessage.CMD + '|' + cmdMessage.numberInQueue + '|' + cmdMessage.morseText;
+        CommandMessageDTO cmdMessage = controller.fetchCommandMessage("Default");
+        String morseCMD = cmdMessage.getCommand() + '|' + cmdMessage.getNumberInQueue() + '|' + cmdMessage.getMorseText();
 
         return morseCMD;
+    }
+
+    public static void main(String[] args){
+        HandlerCreator hc = new HandlerCreator();
+        ServerController sc = new ServerController(hc);
+        MorseMateHandler mm = new MorseMateHandler(sc);
+        ClientMessageDTO cMessage = new ClientMessageDTO("Arbeta Agilt", "Anders Sjögren");
+        ClientMessageDTO cMessage2 = new ClientMessageDTO("Arbeta Agilt IDIOT", "Anders Sjögren");
+
+        sc.addMessage(cMessage);
+        sc.addMessage(cMessage2);
+
+        String cmd = mm.fetchMessage();
+        System.out.println(cmd);
+
+        sc.messageIsRead();
+
+        cmd = mm.fetchMessage();
+        System.out.println(cmd);
     }
 }
