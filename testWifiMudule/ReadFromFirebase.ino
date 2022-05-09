f6c93be75bbf74dc544d2413978f64b8b619dc57
astroxq55 <52320146+astroxq55@users.noreply.github.com>
2022-05-09 10:09:02 +0200

    first test read wifi from firebase

---
+++ testWifiMudule/ReadFromFirebase.ino
@@ -0,0 +1,51 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+// Set these to run example.
+#define FIREBASE_HOST "example.firebaseio.com"
+#define FIREBASE_AUTH "token_or_secret"
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+
+
+void setup() {
+  Serial.begin(9600);
+
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+String n;
+
+void loop() {
+  // set value
+  n=Firebase.getString("logs/message");
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /number failed:");
+      Serial.println(Firebase.error());  
+  } else 
+  {
+      Serial.println(n);
+  }
+  delay(1000);
+}
+
+
+
+
+
+
+
+
+
