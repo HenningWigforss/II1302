2168cdae2703c6018a5911269940f81974aa7887
Murtadha Alobaidi <mhaao@kth.se>
2022-05-30 16:45:54 +0200

    Update example.spec.ts

--- homepage/tests/unit/example.spec.ts
+++ homepage/tests/unit/example.spec.ts
@@ -10,3 +10,17 @@ describe('HomeView.vue', () => {
     expect(wrapper.text()).toMatch(msg)
   })
 })
+
+//import { mount } from '@vue/test-utils'
+//import AboutView from '@/views/AboutView.vue'
+
+//describe("AboutView.vue", () => {
+    //You can call your main function(if you need fined text "GitHub")
+  //  it("Should render Github text", () => {
+    //    const wrapper = mount(AboutView)
+
+      //  expect(wrapper.text()).toContain("Github")
+    //})
+
+
+//})
